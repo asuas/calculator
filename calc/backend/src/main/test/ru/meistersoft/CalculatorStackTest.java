dc84b4f880f2c481f16299f705a76c4708dcf4f6
Anna Semenova <anna.semenova@rtlabs.ru>
2019-02-12 19:13:30 +0300

    templet added

---
+++ calc/backend/src/main/test/ru/meistersoft/CalculatorStackTest.java
@@ -0,0 +1,34 @@
+package ru.meistersoft;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorStackTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void pushValue() {
+    }
+
+    @Test
+    void pushOperator() {
+    }
+
+    @Test
+    void getResult() {
+    }
+
+    @Test
+    void clear() {
+    }
+}
---
+++ calc/backend/src/main/test/ru/meistersoft/CommandExecutorTest.java
@@ -0,0 +1,26 @@
+package ru.meistersoft;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommandExecutorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getState() {
+    }
+
+    @Test
+    void pushCommand() {
+    }
+}
